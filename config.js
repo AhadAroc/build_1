b3358feed288149536074e48d77ab87fa13d16b2
AhadAroc <brothinkshecarti@gmail.com>
2025-02-25 00:07:44 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 module.exports = {
     token: '7511592050:AAH1IMH8kG6UolhwwzIOW-Pf2UUM04hEdTM', // استبدله بتوكن البوت الحقيقي
     dbConfig: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'replays'
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 3306,
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASSWORD || ' ',
+        database: process.env.DB_NAME || 'replays'
     },
     developerIds: new Set(['7308214106'])
 };
