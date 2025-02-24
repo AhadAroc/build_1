1e6f861966d07c9d5e60768d705180fc027691c8
AhadAroc <brothinkshecarti@gmail.com>
2025-02-25 00:13:33 +0300

    Update database.js

--- database.js
+++ database.js
@@ -3,14 +3,17 @@ require('dotenv').config();
 
 const dbConfig = {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    connectTimeout: 60000 // 60 seconds timeout
 };
 
+const pool = mysql.createPool(dbConfig);
 const pool = mysql.createPool(dbConfig);
 
 async function setupDatabase() {
