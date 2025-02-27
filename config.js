ac505a07e254ca56228dd80410613a670e294096
AhadAroc <brothinkshecarti@gmail.com>
2025-02-27 19:16:01 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
-
 module.exports = {
     token: '7511592050:AAH1IMH8kG6UolhwwzIOW-Pf2UUM04hEdTM',
     mongoUri: 'mongodb+srv://Amr:NidisuSI@cluster0.ay6fa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     dbName: 'replays',
-    developerIds: new Set(['7308214106'])
+    developerIds: new Set(['7308214106']),
+    // Include an empty cloudinary object to indicate it's used but not configured
+    cloudinary: {}
 };
